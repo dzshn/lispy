0bc4573b9eb3818cb66ed1f00af9a5fe9f93ae1e
Sofia N. Lima <me@dzshn.xyz>
2022-12-11 06:52:15 -0300

    aoc 09 :3

---
+++ examples/aoc09.py
@@ -0,0 +1,53 @@
+import lispy
+
+(Include@ "str.split")
+(Include@ "set.union")
+
+(Def@ Direction (dir)
+    (If (Eq@ dir, "D")
+        (Tuple@ 1, 0)
+        (If (Eq@ dir, "L")
+            (Tuple@ 0, 1)
+            (If (Eq@ dir, "U")
+                (Tuple@ (Neg@ 1), 0)
+                (Tuple@ 0, (Neg@ 1))))))
+
+(Def@ Clamp (x)
+    (Int (And@ x (Div@ x (Abs@ x)))))
+
+(Def@ Distance (a, b)
+    (Max (Abs (Sub (First@ a) (First@  b)))
+         (Abs (Sub (Second@ a) (Second@ b)))))
+
+(Def@ MoveHead (head, dir)
+    (Tuple (Add (First@  head) (First@  dir))
+           (Add (Second@ head) (Second@ dir))))
+
+(Def@ MoveSegment (tail, head)
+    (If (Ge (Distance@ tail, head), 2)
+        (Tuple (Add (First@  tail) (Clamp (Sub (First@  head) (First@  tail))))
+               (Add (Second@ tail) (Clamp (Sub (Second@ head) (Second@ tail))))),
+        tail))
+
+(Def@ MoveSegments (knots, (n* 0))
+    (If (Eq@ n, (Sub (Length@ knots), 1)),
+        knots,
+        (MoveSegments (Add (Take (Add@ n, 1), knots)
+                           (List (MoveSegment (Nth (Add@ 1, n), knots) (Nth@ n, knots)))
+                           (Drop (Add@ n, 2), knots)),
+            (Add@ 1, n))))
+
+(Def@ Move (knots, dir, times, visited)
+    (If (Eq@ times, 0)
+        (Tuple@ knots, visited)
+        (Let (head (MoveHead (Nth@ 0, knots), dir))
+            (Let (knots (MoveSegments (Add (List@ head) (Drop@ 1, knots))))
+                (Move@ knots, dir, (Sub@ times, 1), (SetUnion@ visited, (Set (Last@ knots))))))))
+
+(Def@ Aoc ((knots* (Mul@ 10, (List@ (Tuple@ 0, 0)))) (visited@ (Set@ (Tuple@ 0, 0))))
+    (Let (line (StrSplit (ReadLine)))
+        (If (Not@ line),
+            visited,
+            (Apply@ Aoc@ (Move@ knots, (Direction@ (First@ line)), (Int@ (Second@ line)), visited)))))
+
+(Length (Aoc))
